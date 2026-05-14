b8c674b84552b749bf0dc6b0032c6b4441216753
Mahdi <mahdi.mahmud2006@gmail.com>
2026-05-15 00:33:29 +0600

    Connecting to public backend tunnel

--- frontend-app/src/utils/api.js
+++ frontend-app/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://early-rules-sit.loca.lt/api';
 
 /**
  * Shorten a URL using the Laravel backend.
